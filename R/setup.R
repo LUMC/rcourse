1a8523a8434bcf91f506a2a288dbf6925f0dc931
Szymon M. Kielbasa <smkielbasa@lumc.nl>
2020-07-17 12:02:46 +0200

    hadley's package emo is optional now

--- R/setup.R
+++ R/setup.R
@@ -1,3 +1,5 @@
+.hasEmo <- suppressWarnings( require( "emo", quietly = TRUE ) )
+
 build_nocode <- function(params) {
   root_dir <- knitr::opts_knit$get("output.dir")
   site_dir <- file.path(root_dir,"_site")
@@ -64,6 +66,22 @@ build_nocode <- function(params) {
   read.csv(file.path(root_dir,"_data",file))
 }
 
+show_color <- function( text, color ) {
+  if (knitr::is_latex_output()) {
+    sprintf("\\textcolor{%s}{%s}", color,  text )
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='color: %s;'>%s</span>", color, text)
+  } else text
+}
+
+show_emoji <- function( id, text = id, color = "red" ) {
+  if( .hasEmo ) {
+    emo::ji( id )
+  } else {
+    show_color( text = paste0( "[", text, "] " ), color = color )
+  }
+}
+
 show_link <- function( url, title, newTab = TRUE ) {
   if( newTab ) {
     paste0( "[", title, "](", url, '){target="_blank"}' )
@@ -72,24 +90,38 @@ show_link <- function( url, title, newTab = TRUE ) {
   }
 }
 
-show_warning <- function( text ) {
+show_menu <- function( menu ) {
   paste0(
-    emo::ji( "warning" ),
+    sapply( menu, function( v ) paste0( "`", v, "`" ) ),
+    collapse = show_emoji( "down_right_arrow", text = "\\", color = "gray" )
+  )
+}
+
+todo <- function( text, color = "yellow" ) {
+  paste0(
+    show_emoji( "construction", text = "TODO", color = color ),
     text
   )
 }
 
-go_extern <- function( url, title, goal ) {
+show_warning <- function( text, color = "orange" ) {
   paste0(
-    emo::ji( "right_arrow" ),
+    show_emoji( "warning", text = "WARNING", color = color ),
+    text
+  )
+}
+
+go_extern <- function( url, title, goal, color = "blue" ) {
+  paste0(
+    show_emoji( "right_arrow", text = "GO TO", color = color ),
     "Go to ", show_link( url = url, title = title ),
     " ", goal, "."
   )
 }
 
-watch_extern_video <- function( url, title, src = NULL ){
+watch_extern_video <- function( url, title, src = NULL, color = "blue" ){
   paste0(
-    emo::ji( "movie_camera" ),
+    show_emoji( "movie_camera", text = "VIDEO", color = color ),
     " Watch ", show_link( url = url, title = title ),
     ( if( is.null( src ) ) "" else paste0( " from ", src ) ),
     "."
