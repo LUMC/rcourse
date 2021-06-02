7a4e800c5b9ded5559d31a4905608ac392d27874
Szymon M. Kiełbasa <smkielbasa@lumc.nl>
2021-06-02 08:13:27 +0200

    more exercises

--- R/Doc.R
+++ R/Doc.R
@@ -1024,7 +1024,8 @@ Renderer <- R6Class(
     writeRmdBodyToc = function( outCon, course ) {
       d <- course$lecturesTibble()
       dd <- d %>%
-        mutate( Session = session.label, Title = lecture.label ) %>%
+        mutate( Session = sprintf( "%s (%s, %s)", session.label, lubridate::stamp( "Sunday, May 1" )( session.date ), session.timeRange ) )%>%
+        mutate( Title = lecture.label ) %>%
         mutate( Lecture = "", Practice = "", Solutions = "" )
       for( idx in seq_len( nrow( dd ) ) ) {
         aDoc <- course$lectureDoc( lectureId = d$lecture.id[[idx]] )
@@ -1109,26 +1110,29 @@ testthat::test_that( "BrightspaceRenderer properties", {
 # ------------------------------------------------------------------------
 
 genTestCourse <- function( testOnly = FALSE ) {
+  startDate <- as.Date( "2021-06-08" )
+  morning <- "09:00-12:30"
+  afternoon <- "13:30-17:00"
   course <- theCourse( id = "Boerhaave_2021_Jun", dir = "BrightspaceTest", label = "LUMC/Boerhaave, June 2021: R for data analysis" )
   course <- course$add(
-    session( id = "slot1", label = "R and RStudio basics" ) %>%
-      add( lecture( id = "index", label = "Course Introduction", hasTasks = FALSE, min = 5 ) ) %>%
-      add( lecture( id = "introduction0", label = "R Introduction", hasTasks = FALSE, min = 30 ) ) %>%
-      add( lecture( id = "basic_calculator0", label = "Calculator", min = 45 ) ) %>%
-      add( lecture( id = "basic_variables0", label = "Variables", min = 45 ) ) %>%
-      add( lecture( id = "course_data0", label = "Example data (pulse, survey)", hasTasks = FALSE, min = 5 ) ) %>%
-      add( lecture( id = "basic_projects0", label = "Projects", hasTasks = FALSE, min = 45 ) ) %>%
-      add( lecture( id = "basic_scripts0", label = "Scripts", hasTasks = FALSE, min = 45 ) )
+    session( id = "slot1", label = "R and RStudio basics", date = startDate, timeRange = morning ) %>%
+      add( lecture( id = "index", label = "Course Introduction", hasTasks = FALSE, min = 15 ) ) %>%
+      add( lecture( id = "introduction0", label = "R Introduction", hasTasks = FALSE, min = 10 ) ) %>%
+      add( lecture( id = "basic_calculator0", label = "Calculator", min = 25 ) ) %>%
+      add( lecture( id = "basic_variables0", label = "Variables", min = 25, pauseMin = 30 ) ) %>%
+      add( lecture( id = "course_data0", label = "Example data (pulse, survey)", hasTasks = FALSE, min = 10 ) ) %>%
+      add( lecture( id = "basic_projects0", label = "Projects", hasTasks = FALSE, min = 20 ) ) %>%
+      add( lecture( id = "basic_scripts0", label = "Scripts", hasTasks = FALSE, min = 60 ) )
   )
   if( !testOnly ) {
     course <- course$add(
-      session( id = "slot2", label = "Data structures 1/2" ) %>%
-        add( lecture( id = "basic_vectors0", label = "Vectors", min = 45 ) ) %>%
-        add( lecture( id = "basic_factors0", label = "Factors", min = 30 ) ) %>%
+      session( id = "slot2", label = "Data structures 1/2", date = startDate, timeRange = afternoon ) %>%
+        add( lecture( id = "basic_vectors0", label = "Vectors", min = 120, pauseMin = 30 ) ) %>%
+        add( lecture( id = "basic_factors0", label = "Factors", min = 60 ) ) %>%
         add( lecture( id = "packages0", label = "Install/use packages", hasTasks = FALSE, min = 30 ) )
     )
     course <- course$add(
-      session( id = "slot3", label = "Data manipulation 1/3" ) %>%
+      session( id = "slot3", label = "Data manipulation 1/3", date = startDate + 1, timeRange = morning ) %>%
         add( lecture( id = "tidyverse0", label = "Tidyverse library", hasTasks = FALSE, min = 45 ) ) %>%
         add( lecture( id = "dplyr_tibble0", label = "Tibble", min = 30 ) ) %>%
         add( lecture( id = "dplyr_select0", label = "Select variables/cols", min = 30 ) ) %>%
@@ -1136,30 +1140,33 @@ genTestCourse <- function( testOnly = FALSE ) {
         add( lecture( id = "dplyr_mutate0", label = "Add/modify variables", min = 30 ) )
     )
     course <- course$add(
-      session( id = "slot4", label = "Data manipulation 2/3" ) %>%
+      session( id = "slot4", label = "Data manipulation 2/3", date = startDate + 1, timeRange = afternoon ) %>%
         add( lecture( id = "dplyr_pipe0", label = "Pipe operator", min = 45 ) ) %>%
         add( lecture( id = "dplyr_summarise0", label = "Summarise", min = 30 ) ) %>%
         add( lecture( id = "dplyr_group0", label = "Groups", min = 30 ) )
     )
     course <- course$add(
-      session( id = "slot5", label = "Data structures 2/2" ) %>%
+      session( id = "slot5", label = "Data structures 2/2", date = startDate + 2, timeRange = morning ) %>%
         add( lecture( id = "basic_lists0", label = "Lists", min = 45 ) ) %>%
         add( lecture( id = "basic_formulas0", label = "Formulas", min = 30 ) ) %>%
         add( lecture( id = "basic_matrices0", label = "Matrices", min = 30 ) )
     )
     course <- course$add(
-      session( id = "slot6", label = "Graphics" ) %>%
+      session( id = "slot6", label = "Graphics", date = startDate + 2, timeRange = afternoon ) %>%
         add( lecture( id = "ggplot_basics0", label = "Plots/ggplot2", min = 45 ) ) %>%
         add( lecture( id = "ggplot_scales0", label = "Plot axes/scales", min = 30 ) ) %>%
         add( lecture( id = "ggplot_facets_themes0", label = "Plot panels/facets", min = 30 ) ) %>%
         add( lecture( id = "ggplot_geoms0", label = "Plot types", hasTasks = FALSE, min = 30 ) )
     )
     course <- course$add(
-      session( id = "slot7", label = "Data manipulation 3/3 and functions" ) %>%
+      session( id = "slot7", label = "Data manipulation 3/3 and functions", date = startDate + 3, timeRange = morning ) %>%
         add( lecture( id = "dplyr_join0", label = "Merging/joining tables", min = 45 ) ) %>%
         add( lecture( id = "tidyr_reshape0", label = "Reshaping tables", min = 30 ) ) %>%
         add( lecture( id = "advanced_user_functions0", label = "User-defined functions", min = 30 ) ) %>%
         add( lecture( id = "useful_functions0", label = "Useful R functions", hasTasks = FALSE, min = 30 ) )
+    )
+    course <- course$add(
+      session( id = "slot8", label = "Self-study assignment", date = startDate + 3, timeRange = afternoon )
     )
   }
   course <- course$add(
